cdae3fa1e0acf441215ea677210ed6a39abdf28f
Steven Mak <14815068+MelonShooter@users.noreply.github.com>
2022-03-08 02:24:45 -0800

    Add other setup commands to install script

--- install.sh
+++ install.sh
@@ -3,5 +3,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
 stack build
-sudo cp "$(stack path --local-install-root)/bin/qriollo" /usr/bin/qr
+sudo install -Dm755 "$(stack path --local-install-root)/bin/qriollo" "/usr/bin/qr"
+sudo install -Dm666 Chamuyo.q "/usr/share/qriollo/Chamuyo.q"
+echo "export RUTA_QRIOLLO=".:/usr/share/qriollo"" > qriollo.sh
+sudo install qriollo.sh -Dm755 /etc/profile.d/qriollo.sh
 stack clean
+rm qriollo.sh
